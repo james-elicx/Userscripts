2d501ec412f1a640f5d47d284ffe98c9b65bbee9
James <james@eli.cx>
2020-06-12 11:50:42 +0100

    Update HF Anti-Caps Lock.user.js

--- HF Anti-Caps Lock.user.js
+++ HF Anti-Caps Lock.user.js
@@ -8,7 +8,6 @@
 // @include      https://hackforums.net/forumdisplay.php?*
 // @updateURL    https://github.com/moodiest/Userscripts/raw/master/HF%20Anti-Caps%20Lock.user.js
 // @downloadURL  https://github.com/moodiest/Userscripts/raw/master/HF%20Anti-Caps%20Lock.user.js
-// @match        https://*.hackforums.net/*
 // @grant        none
 // ==/UserScript==
 //
