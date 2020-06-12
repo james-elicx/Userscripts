09b64f7f199d813091c2766a0eb79ce5e844fbdc
James <james@eli.cx>
2020-06-12 12:02:29 +0100

    Create HF API Add Forum Link.user.js

---
+++ HF API Add Forum Link.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         HF API Add Forum Link
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add a forum link to the API subforum on the API pages in the user control panel.
+// @author       James
+// @include      https://hackforums.net/usercp.php?action=api*
+// @updateURL    https://github.com/moodiest/Userscripts/raw/master/
+// @downloadURL  https://github.com/moodiest/Userscripts/raw/master/
+// @grant        none
+// ==/UserScript==
+//
+// ------- CHANGE LOG -------
+// Initial release.
+// ---------------------------
+
+(function() {
+    'use strict';
+
+    let menuButtons = document.querySelector(".oc-item .float_right");
+
+    $(menuButtons).prepend("<a href=\"forumdisplay.php?fid=375\" class=\"button\" title=\"Go to the API forum.\" style=\"margin-right:4px;\"><i class=\"fas fa-edit\"></i><span style=\"margin-left: 8px; font-weight: 500;\">API Forum</span></a>");
+
+})();
