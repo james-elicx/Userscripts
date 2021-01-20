4c04bd64a61fb29360d4e4a1e60be820c9ea6c45
James <james@eli.cx>
2021-01-20 18:42:16 +0000

    Create HF Pink Theme.user.js

---
+++ HF Pink Theme.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         HF: Pink Theme
+// @description  Pink theme for Hack Forums.
+// @version      0.0.1
+// @author       James
+// @updateURL    https://github.com/moodiest/Userscripts/raw/master/HF%20Pink%20Theme.user.js
+// @downloadURL  https://github.com/moodiest/Userscripts/raw/master/HF%20Pink%20Theme.user.js
+// @iconURL      https://github.com/moodiest/Userscripts/raw/master/icon.jpg
+// @run-at       document-start
+// @include      https://hackforums.net*
+// @grant        none
+// ==/UserScript==
+
+var Pink = {
+    init: function () {
+        Pink.addCSS();
+        Pink.addLogo();
+    },
+
+    addCSS: function () {
+        let styleElement = `<style>
+body {
+    background: #0e0e0e url(https://raw.githubusercontent.com/moodiest/Userscripts/master/HF%20Pink%20Theme/mosaic.png) fixed !important;
+}
+
+#container {
+    background: rgb(11 5 11 / 70%) !important;
+}
+
+.thead, .shadetabs li a.selected, .pagination .pagination_current, .quickthread_button {
+    background: #bd4aa7 !important;
+}
+
+.logo-hide-button {
+    color: #bd4aa7 !important;
+}
+</style>`;
+        (document.head || document.documentElement).insertAdjacentHTML('beforeend', styleElement);
+    },
+
+    addLogo: function () {
+        new MutationObserver(function(mutations) {
+            if (document.getElementsByTagName('img')[0]) {
+                let logo = document.querySelector('#logo > div.wrapper > a > img');
+                if (logo) {
+                    logo.src = `https://raw.githubusercontent.com/moodiest/Userscripts/master/HF%20Pink%20Theme/logo.png`;
+                    this.disconnect();
+                }
+            }
+        }).observe(document, {childList: true, subtree: true});
+    }
+}
+
+Pink.init();
