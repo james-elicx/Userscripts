125ba191554693b56afdac318cbdeb6815a2f514
James <james@eli.cx>
2021-07-04 03:08:31 +0100

    Update HF Wiki Generate Article to Clipboard.user.js
    Update to support generate award recipient tables with either HF profile links or wiki user article links.

--- HF Wiki Generate Article to Clipboard.user.js
+++ HF Wiki Generate Article to Clipboard.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HF Wiki: Generate Article to Clipboard
-// @version      0.0.1
+// @version      0.0.2
 // @description  Generate basic articles to your clipboard for the HF Wiki.
 // @author       James
 // @updateURL    https://github.com/moodiest/Userscripts/raw/master/HF%20Wiki%20Generate%20Article%20to%20Clipboard.user.js
@@ -15,6 +15,8 @@ var Gen = {
   init: function () {
       if (document.URL.includes(`member.php?action=profile`)) {
           Gen.addProfileBtn();
+      } else if (document.URL.includes(`myawards.php?awid=`)) {
+          Gen.addAwardRecipientsBtn();
       } else if (document.URL.includes(`myawards.php`)) {
           Gen.addAwardBtn();
       }
@@ -135,6 +137,66 @@ var Gen = {
 
   awardGenerationCallback: function (awardId) {
       var article = document.getElementById(`userscript-gen-wiki-article-text-${awardId}`).getAttribute(`value`);
+
+      GM_setClipboard(article);
+  },
+
+  addAwardRecipientsBtn: function () {
+      var template = `{{{!}} class="wikitable"
+{{!}}-
+! Recipient Name !! Reason !! Date Given
+{{!}}-%row%
+{{!}}}`;
+
+      var awardHeader = document.querySelector(`div.wrapper > form > table > tbody > tr > td.thead > strong`);
+
+      awardHeader.insertAdjacentHTML(`afterend`, `<td class="trow1" align="center"><a href="javascript:void(0);" id="userscript-gen-wiki-article-btn1" data-tooltip="HF Wiki: Generate Recipients to Clipboard - User Article Link" title="HF Wiki: Generate Recipients to Clipboard - User Article Link" style="padding: 0px 15px;"><i class="fab fa-wikipedia-w fa-lg" aria-hidden="true"></i></a></td>`);
+      document.getElementById(`userscript-gen-wiki-article-btn1`).insertAdjacentHTML(`afterbegin`, `<input type="text" id="userscript-gen-wiki-article-text1" style="display: none;" value="">`);
+
+      awardHeader.insertAdjacentHTML(`afterend`, `<td class="trow1" align="center"><a href="javascript:void(0);" id="userscript-gen-wiki-article-btn2" data-tooltip="HF Wiki: Generate Recipients to Clipboard - User Profile Link" title="HF Wiki: Generate Recipients to Clipboard - User Profile Link" style="padding: 0px 15px;"><i class="fab fa-wikipedia-w fa-lg" aria-hidden="true"></i></a></td>`);
+      document.getElementById(`userscript-gen-wiki-article-btn2`).insertAdjacentHTML(`afterbegin`, `<input type="text" id="userscript-gen-wiki-article-text2" style="display: none;" value="">`);
+
+      var article1 = template;
+      var article2 = template;
+
+      document.querySelectorAll(`#content > div > form > table > tbody > tr:nth-child(n+3)`).forEach((userRow, i) => {
+          var rowTemplate1 = `
+{{!}} [[%username%]] {{!}}{{!}} %reason% {{!}}{{!}} %date%
+{{!}}-%row%`;
+          var rowTemplate2 = `
+{{!}} [https://hackforums.net/member.php?action=profile&uid=%uid% '''%username%'''] {{!}}{{!}} %reason% {{!}}{{!}} %date%
+{{!}}-%row%`;
+
+          var uid = userRow.querySelector(`td:nth-child(1) > strong > a`).href.split(`uid=`)[1];
+          var username = userRow.querySelector(`td:nth-child(1) > strong > a > span`).textContent;
+          var reason = userRow.querySelector(`td:nth-child(2)`).textContent;
+          var date = userRow.querySelector(`td:nth-child(4)`).textContent;
+
+          rowTemplate1 = rowTemplate1.replace(`%username%`, username);
+          rowTemplate1 = rowTemplate1.replace(`%reason%`, reason);
+          rowTemplate1 = rowTemplate1.replace(`%date%`, date);
+
+          rowTemplate2 = rowTemplate2.replace(`%uid%`, uid);
+          rowTemplate2 = rowTemplate2.replace(`%username%`, username);
+          rowTemplate2 = rowTemplate2.replace(`%reason%`, reason);
+          rowTemplate2 = rowTemplate2.replace(`%date%`, date);
+
+          article1 = article1.replace(`%row%`, rowTemplate1);
+          article2 = article2.replace(`%row%`, rowTemplate2);
+      });
+
+      article1 = article1.replace(`%row%`, ``);
+      article2 = article2.replace(`%row%`, ``);
+
+      document.getElementById(`userscript-gen-wiki-article-text1`).setAttribute(`value`, `${article1}`);
+      document.getElementById(`userscript-gen-wiki-article-text2`).setAttribute(`value`, `${article2}`);
+
+      document.getElementById(`userscript-gen-wiki-article-btn1`).addEventListener(`click`, function () { Gen.awardRecipientsGenerationCallback(`1`) });
+      document.getElementById(`userscript-gen-wiki-article-btn2`).addEventListener(`click`, function () { Gen.awardRecipientsGenerationCallback(`2`) });
+  },
+
+  awardRecipientsGenerationCallback: function (type) {
+      var article = document.getElementById(`userscript-gen-wiki-article-text${type}`).getAttribute(`value`);
 
       GM_setClipboard(article);
   }
